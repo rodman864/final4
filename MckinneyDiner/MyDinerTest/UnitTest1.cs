ba6984ed4a409976895f6c84c692433c24074e61
Rodrigus Mckinney <rodrigusmckinney@gmail.com>
2021-02-08 10:56:36 -0700

    no message

---
+++ MckinneyDiner/MyDinerTest/UnitTest1.cs
@@ -0,0 +1,42 @@
+using Diner;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Diner.MckinneyDiner;
+
+namespace MyDinerTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ShowCustomer_isWaiting_ReturnTrue()
+        {
+            //Arrange
+            MckinneyDiner inQue = new MckinneyDiner();
+
+            InWaiting newCustomer = new InWaiting();
+            newCustomer.IsAdded = true;
+            newCustomer.IsDishClean = false;
+            newCustomer.CustomerName = "Tracy";
+
+
+            inQue.CurrentlyWaiting = newCustomer;
+
+
+
+            //Act
+            InWaiting newPerson = new InWaiting();
+            newPerson.IsAdded = true;
+            newPerson.IsDishDirty = true;
+            newPerson.CustomerName = "Amos";
+
+            inQue.CurrentlyWaiting = newPerson;
+
+            bool showCustomer = inQue.showCustomer(newPerson);
+
+
+            //Assert
+            Assert.IsTrue(showCustomer);
+
+        }
+    }
+}
---
+++ MckinneyDiner/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Diner
+{
+    public class MckinneyDiner
+    {
+        public InWaiting CurrentlyWaiting { get; set; }
+        private Queue<int> waitingQueue = new Queue<int>();
+
+        public void addCustomerToQueue(int parameter)
+        {
+            waitingQueue.Enqueue(parameter);
+
+        }
+
+        public bool showCustomer(InWaiting inWaiting)
+        {
+            Console.Write("This is the amount of customers that need to be seated: " +
+                                 waitingQueue.Count);
+
+            if (inWaiting.IsAdded)
+                return true;
+
+            if (inWaiting.IsAdded != CurrentlyWaiting.IsAdded)
+                return true;
+
+            return false;
+
+        }
+        public void actualCustomer()
+        {
+            foreach (var item in waitingQueue)
+            {
+                Console.WriteLine("Customer: " + item);
+            }
+        }
+        public void CustomerSeated()
+        {
+            waitingQueue.Dequeue();
+        }
+        private Stack<String> DirtyDish = new Stack<string>();
+        private Stack<string> CleanDish = new Stack<string>();
+
+        public void CustomerDishes(string Dishes)
+        {
+            DirtyDish.Push(Dishes);
+
+        }
+        public void CleanTheDishes()
+        {
+            string lastCustomer = DirtyDish.Pop();
+            CleanDish.Push(lastCustomer);
+        }
+        public void CleanPile()
+        {
+            foreach(var item in  CleanDish)
+            {
+                Console.WriteLine("The dishes are put back:" + item);
+            }
+        }
+
+        public void DishClean()
+        {
+
+        }
+        public void DishDirty()
+        {
+
+        }
+        public class InWaiting
+        {
+            public string CustomerName { get; set; }
+            public bool IsAdded { get; set; }
+            public bool IsDishDirty { get; set; }
+            public bool IsDishClean { get; set; }
+          
+            
+
+        }
+        public class AddedToQue
+        {
+            public string isAddedToQue { get; set; }
+        }
+       
+        
+
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            MckinneyDiner restaurantCustomer = new MckinneyDiner();
+            restaurantCustomer.addCustomerToQueue(1);
+            restaurantCustomer.addCustomerToQueue(2);
+            restaurantCustomer.addCustomerToQueue(3);
+            restaurantCustomer.addCustomerToQueue(4);
+            restaurantCustomer.addCustomerToQueue(5);
+            restaurantCustomer.addCustomerToQueue(6);
+
+            restaurantCustomer.CustomerSeated();
+
+            restaurantCustomer.actualCustomer();
+
+
+
+
+        
+            restaurantCustomer.CustomerDishes("dish1");
+            restaurantCustomer.CustomerDishes("dish2");
+            restaurantCustomer.CustomerDishes("dish3");
+
+            restaurantCustomer.CleanTheDishes();
+            restaurantCustomer.CleanPile();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+    
+
+}
+
